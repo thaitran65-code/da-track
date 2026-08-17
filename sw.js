bf11ecb639ce056a699b94126578ef6d2e962026
thaitran65-code <thaitran65@gmail.com>
2026-08-17 14:28:27 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'datrack-v1';
+const ASSETS = ['multitrack_player.html','manifest.json','icon-192.png','icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{})); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
